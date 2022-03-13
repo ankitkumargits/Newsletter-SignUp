f31c55e2b4cfbaf6e5da1ed3db78766dd2ee4115
Ankit kumar <jitusablaniya@gmail.com>
2022-03-13 12:03:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+// JShint esversion 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ] 
+    }
+
+    const jsonData = JSON.stringify(data);
+
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/8c6d1fb330";
+
+    const options = {
+        method: "POST",
+        auth: "ankit1:3ee0a3ddef5dc6dc1b0fa8eaa384e072-us1"
+    }
+
+    const request = https.request(url, options, function(response){
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+
+        }
+        response.on("data", function(data){
+        console.log(JSON.parse(data));
+        });
+    });
+
+        request.write(jsonData);
+        request.end();
+});
+
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000");
+});
+
+
+
+// API Key
+// 3ee0a3ddef5dc6dc1b0fa8eaa384e072-us14
+
+// Audience id (Unique id ) or List id 
+// 8c6d1fb330
+
